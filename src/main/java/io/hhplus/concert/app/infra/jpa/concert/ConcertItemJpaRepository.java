afcafb73b99c1cfd7a337a0befa36fccfb777bb6
riley-hhp <riley.hhp@gmail.com>
2024-10-25 09:33:20 +0900

    feature(STEP10): 동시성 테스트, 회고록

--- src/main/java/io/hhplus/concert/app/infra/jpa/concert/ConcertItemJpaRepository.java
+++ src/main/java/io/hhplus/concert/app/infra/jpa/concert/ConcertItemJpaRepository.java
@@ -1,14 +1,23 @@
 package io.hhplus.concert.app.infra.jpa.concert;
 
 import io.hhplus.concert.app.domain.concert.ConcertItem;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ConcertItemJpaRepository extends JpaRepository<ConcertItem, Long> {
 
     List<ConcertItem> findByConcertIdAndSessionAtAfterAndCapacityGreaterThan(long concertId, LocalDateTime now, int capacity);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT ci FROM ConcertItem ci WHERE ci.id = :concertItemId")
+    Optional<ConcertItem> findConcertItemForUpdate(@Param("concertItemId") Long concertItemId);
 }
